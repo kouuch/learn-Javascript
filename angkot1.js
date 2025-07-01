f6a08b829eee0f9eed1f36f8e334d3d1eae555db
kouuch <rifkyirsada2@gmail.com>
2025-07-02 03:16:17 +0800

    next

--- angkot1.js
+++ angkot1.js
@@ -1,4 +1,4 @@
-// foor loop
+// while loop bag 1
 
 // var angkot = 1
 
@@ -8,6 +8,17 @@
 // }
 
 // foor loop bag 2
-for (i = 0; i<10; i++){
+for (i= 1; i<=10 ; i++){
     console.log(`angkot No. ${i} beroperasi dengan baik.`)
 }
+
+// bervariabel
+// var i = 0
+// for (i; i<10; i++){
+//     console.log(`angkot No. ${i} beroperasi dengan baik.`)
+// }
+
+// Do-While Loop
+
+
+
