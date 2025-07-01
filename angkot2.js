8fb83110432111106f386b4f4d7aaafdefeb261d
kouuch <rifkyirsada2@gmail.com>
2025-07-02 03:24:01 +0800

    angkot tidak beroperasi

--- angkot2.js
+++ angkot2.js
@@ -6,3 +6,7 @@ while(noAngkot <= angkotBeroperasi){
     console.log('Angkot No. ' +noAngkot + ' beroperasi dengan baik.')
     noAngkot++
 }
+
+for(noAngkot = angkotBeroperasi +1; noAngkot<=jmlAngkot; noAngkot++){
+    console.log('Angkot No. ' +noAngkot + ' todak beroperasi dengan baik.')
+}
