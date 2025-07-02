b4e3a3494fc25f6191f97ec8f6baaabea26c7bff
kouuch <rifkyirsada2@gmail.com>
2025-07-02 14:38:14 +0800

    for if ...

---
+++ angkot3.js
@@ -0,0 +1,10 @@
+var jmlAngkot = 10
+var angkotBeroperasi = 6
+// for if else
+for(var noAngkot = 1; noAngkot<= jmlAngkot; noAngkot++){
+    if(noAngkot <= angkotBeroperasi){
+        console.log(`Angkot No. ${noAngkot} sedang beroperasi dengan baik.`)
+    } else {
+        console.log(`Angkot No. ${noAngkot} sedang tidak beroperasi dengan baik.`)
+    }
+}
