7a8e66dee62f10c227226cb83a3807fd26e2595a
kouuch <rifkyirsada2@gmail.com>
2025-07-02 22:17:48 +0800

    cetak bintang miring

---
+++ star.js
@@ -0,0 +1,14 @@
+var star = ''
+
+for(var i = 0; i<5; i++){
+    star += '*'
+}
+console.log (star)
+
+// output 1 : star += '*'
+// *****
+
+// output 2 : star += '\n'
+
+
+// star += '*' sama saja dengan star = star + '*'
