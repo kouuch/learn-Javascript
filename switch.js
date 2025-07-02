cb1c40271727150676e58fea432bc31289900c66
kouuch <rifkyirsada2@gmail.com>
2025-07-02 20:33:10 +0800

    switch mirip mirip

---
+++ switch.js
@@ -0,0 +1,9 @@
+var angka = prompt('masukan angka 1 : ')
+
+if (angka == 1) {
+    alert('anda memasukan angka yang tepat')
+} else {
+    alert('anda salah memasukan angka')
+}
+
+// ini sejenis switch mirip tapi bukan switch
