c9a06900a1b6317595ffeb570a6f5e31a94c73cd
kouuch <rifkyirsada2@gmail.com>
2025-07-04 22:53:42 +0800

    function

---
+++ funcition/app.js
@@ -0,0 +1,20 @@
+function ohayo(name) {
+    console.log(`hi ${name} selamat pagi`)
+}
+ohayo('rifky')
+//  ohayo parameter
+// 'rifky' argument
+
+
+function tambahan(a, b) {
+    const total = a + b
+    console.log(total)
+}
+tambahan(10, 11)
+
+// dadu
+function dadu(){
+    console.log(Math.floor(Math.random() *6)+1)
+}
+dadu()
+
