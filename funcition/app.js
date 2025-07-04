fb7ba574427946ed53b5d842bd38bbc602676cfd
kouuch <rifkyirsada2@gmail.com>
2025-07-04 22:55:55 +0800

    scope global, local, dan block

--- funcition/app.js
+++ funcition/app.js
@@ -18,3 +18,39 @@ function dadu(){
 }
 dadu()
 
+//scope 
+
+let bahasa = 'indonesia';  // Variabel dengan global scope
+
+function typeBahasa() {
+    let bahasa = 'inggris';  // Variabel dengan local scope (hanya bisa diakses dalam fungsi typeBahasa)
+    console.log(bahasa);  // Output: inggris (mengakses variabel lokal dalam fungsi)
+}
+
+console.log(bahasa);  // Output: indonesia (mengakses variabel global)
+
+typeBahasa();  // Output: inggris (mengakses variabel lokal dalam fungsi)
+e
+
+function typeBahasaGlobal() {
+    console.log(bahasaGlobal);  // Mengakses variabel global
+}
+
+typeBahasaGlobal();  // Output: indonesia
+
+
+// scope local
+function typeBahasa() {
+    let bahasa = 'inggris';  // Local Scope (fungsi)
+    console.log(bahasa);  // Mengakses variabel lokal (inggris)
+}
+
+typeBahasa();  // Output: inggris
+
+
+// block scope
+if (true) {
+    let bahasa = 'perancis';  // Block Scope
+    console.log(bahasa);  // Output: perancis
+}
+console.log(bahasa);  // Error: bahasa is not defined (karena berada di dalam block)
