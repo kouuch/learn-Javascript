539fc1416fc0fdabec0d87e636c225962aaa9da6
kouuch <rifkyirsada2@gmail.com>
2025-07-06 00:10:30 +0800

    callback dfengan funcrtion

--- caalback/app.js
+++ caalback/app.js
@@ -9,7 +9,34 @@ const number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 // callback
 
-number.forEach(function(el){
+number.forEach(function(el){ //exverison function
     if(el % 2 === 0)
     console.log(el)
 })
+
+// map
+const numberDoubel = number.map(function(num){
+    return num * 2
+})
+
+
+/*
+number
+(9) [1, 2, 3, 4, 5, 6, 7, 8, 9]
+numberDoubel
+(9) [2, 4, 6, 8, 10, 12, 14, 16, 18]
+*/
+
+
+function perpangkatan(x){ //functio umum
+    return x * x //nilaai balik
+}
+
+const hasil = perpangkatan(5) //tempata meyimpan nilai dari perpangkatan
+
+
+
+//arrow
+const perpangkatan = (x) =>{ //parameter    (x)      
+    return x * x
+}
