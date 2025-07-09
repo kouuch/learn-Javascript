581d8ffeedf9000374ac0a3c1477265e3c95b5a0
kouuch <rifkyirsada2@gmail.com>
2025-07-10 01:07:31 +0800

    Update color.js

--- asyncawaait/promise/color.js
+++ asyncawaait/promise/color.js
@@ -1,6 +1,6 @@
 const delayColorChange = (color, delay) => {
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
             document.body.style.backgroundColor = color
             resolve()
         }, delay)
@@ -11,7 +11,8 @@ const delayColorChange = (color, delay) => {
 // .then(()=> delayColorChange('#3D74B6', 1000))
 // .then(()=> delayColorChange('#9929EA', 1000))
 
-const changeColor = async () => { //Arrow Function
+const changeColors = async () => { //Arrow Function
+    await delayColorChange('tomato', 1000);
     await delayColorChange('#3D74B6', 1000);
     await delayColorChange('#9929EA', 1000);
     return 'Done Cuy';
@@ -20,24 +21,26 @@ const changeColor = async () => { //Arrow Function
 Digunakan ketika ingin sintaksis lebih ringkas dan tidak membutuhkan akses ke this atau arguments
 */
 
-// const changeColor = async function() { //function expression
-//     await delayColorChange('#3D74B6', 1000);
-//     await delayColorChange('#9929EA', 1000);
-//     return 'Done Cuy';
-// }
+const changeColorss = async function () { //function expression
+    await delayColorChange('tomato', 1000);
+    await delayColorChange('#3D74B6', 1000);
+    await delayColorChange('#9929EA', 1000);
+    return 'Done Cuy';
+}
 
 /*
 Digunakan ketika fungsi harus disimpan dalam variabel, misalnya dalam callback atau event handler
 */
 
-// async function changeColor(){ //Function Declaration
-//     await delayColorChange('#3D74B6', 1000)
-//     await delayColorChange('#9929EA', 1000)
-//     return 'Done Cuy'
-// }
+ async function changeColor(){ //Function Declaration
+    await delayColorChange('tomato', 1000);
+    await delayColorChange('#3D74B6', 1000)
+    await delayColorChange('#9929EA', 1000)
+    return 'Done Cuy'
+}
 
 /*
 Digunakan ketika fungsi akan dipanggil di seluruh lingkup atau di awal kode
 */
 
-changeColor().then((res)=> alert(res))
+changeColorss().then((res) => alert(res))
