af603a2c36719232c9acb29d2e03a4cd0d458e75
kouuch <rifkyirsada2@gmail.com>
2025-07-10 01:05:24 +0800

    function dan kegunaannya

--- asyncawaait/promise/color.js
+++ asyncawaait/promise/color.js
@@ -11,10 +11,33 @@ const delayColorChange = (color, delay) => {
 // .then(()=> delayColorChange('#3D74B6', 1000))
 // .then(()=> delayColorChange('#9929EA', 1000))
 
-async function changeColor(){
-    await delayColorChange('#3D74B6', 1000)
-    await delayColorChange('#9929EA', 1000)
-    return 'Done Cuy'
+const changeColor = async () => { //Arrow Function
+    await delayColorChange('#3D74B6', 1000);
+    await delayColorChange('#9929EA', 1000);
+    return 'Done Cuy';
 }
+/*
+Digunakan ketika ingin sintaksis lebih ringkas dan tidak membutuhkan akses ke this atau arguments
+*/
+
+// const changeColor = async function() { //function expression
+//     await delayColorChange('#3D74B6', 1000);
+//     await delayColorChange('#9929EA', 1000);
+//     return 'Done Cuy';
+// }
+
+/*
+Digunakan ketika fungsi harus disimpan dalam variabel, misalnya dalam callback atau event handler
+*/
+
+// async function changeColor(){ //Function Declaration
+//     await delayColorChange('#3D74B6', 1000)
+//     await delayColorChange('#9929EA', 1000)
+//     return 'Done Cuy'
+// }
+
+/*
+Digunakan ketika fungsi akan dipanggil di seluruh lingkup atau di awal kode
+*/
 
 changeColor().then((res)=> alert(res))
